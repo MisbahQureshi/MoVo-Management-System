61f457454c7cf5d2fd7c077a2633f342d605b346
Sameer Akhtari <sameer_akhtari@hotmail.com>
2024-08-31 13:40:53 +0000

    .

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 from dotenv import load_dotenv
-load_dotenv()  # This loads the variables from .env
-
-import os
-print("MONGO_URI from env:", os.environ.get('MONGO_URI'))
-
 from app import create_app
+import os
 
+load_dotenv()  # This loads the variables from .env
+print("MONGO_URI from env:", os.environ.get('MONGO_URI'))
+print("All environment variables:", os.environ)
 app = create_app()
 
 if __name__ == '__main__':
